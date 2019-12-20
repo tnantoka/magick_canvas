16c9016231cf8901223c9b6b28f22e1bc61f2860
tnantoka <tnantoka@bornneet.com>
2019-12-20 21:07:55 +0900

    bundle gem magick_canvas

---
+++ lib/magick_canvas.rb
@@ -0,0 +1,6 @@
+require "magick_canvas/version"
+
+module MagickCanvas
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/magick_canvas/version.rb
@@ -0,0 +1,3 @@
+module MagickCanvas
+  VERSION = "0.1.0"
+end
